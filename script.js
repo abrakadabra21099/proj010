2fff63fc9a5bc27b509234369a03d2881b5981ed
4i! aka /bin/bashrrY <abrakadabra21099@gmail.com>
2021-04-07 16:26:27 +0400

    Learn 32. Done.

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
 'use strict';
 
-const btns = document.querySelectorAll('button'),
-    overlay = document.querySelector('.overlay');
-// console.log(btn);
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastChild);
+// console.log(document.body.lastElementChild);
 
-// btn.addEventListener('click', () => {
-//     alert('Click');
-// });
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('#current').parentElement);
+// console.log(document.querySelector('[data-current="3"]').previousElementSibling);
 
-const deleteElement = (e) => {
-    console.log(e.currentTarget);
-    console.log(e.type);
-    // e.target.remove();
-    // e.target.removeEventListener
-    // btn.removeEventListener('click', deleteElement)
-};
-// btn.addEventListener('click', deleteElement);
-// overlay.addEventListener('click', deleteElement);
-
-btns.forEach(btn => {
-    btn.addEventListener('click', deleteElement, {once: true});
-});
-
-const link = document.querySelector('a');
-link.addEventListener('click', (event) => {
-    event.preventDefault();
-    console.log(event.target)
-});
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {continue;}
+    console.log(node);
+}

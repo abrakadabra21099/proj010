401060f8f7ae29036868c72799d8b9809d3deb4c
4i! aka /bin/bashrrY <abrakadabra21099@gmail.com>
2021-04-07 15:52:06 +0400

    Learn 31. Done.

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 'use strict';
 
+const btns = document.querySelectorAll('button'),
+    overlay = document.querySelector('.overlay');
+// console.log(btn);
+
+// btn.addEventListener('click', () => {
+//     alert('Click');
+// });
+
+const deleteElement = (e) => {
+    console.log(e.currentTarget);
+    console.log(e.type);
+    // e.target.remove();
+    // e.target.removeEventListener
+    // btn.removeEventListener('click', deleteElement)
+};
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+
+btns.forEach(btn => {
+    btn.addEventListener('click', deleteElement, {once: true});
+});
+
+const link = document.querySelector('a');
+link.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(event.target)
+});
